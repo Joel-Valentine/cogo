fcf33eacf1f5284142a9a30f5bcbf0e94f67bf71
Joel Valentine <joel@opteo.com>
2020-04-13 18:19:30 +0100

    chore: 2.2.3

--- version/version.go
+++ version/version.go
@@ -9,7 +9,7 @@ import (
 var GitCommit string
 
 // Version returns the main version number that is being run at the moment.
-const Version = "2.1.0"
+const Version = "2.2.3"
 
 // BuildDate returns the date the binary was built
 var BuildDate = ""
